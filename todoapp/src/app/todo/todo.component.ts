17776d85bc5b47e3b6b0dd25d73fdd8f3e420373
ahmetyanik <ahmetyank4242@gmail.com>
2022-11-05 20:46:33 +0100

    lists are ready

--- todoapp/src/app/todo/todo.component.ts
+++ todoapp/src/app/todo/todo.component.ts
@@ -9,7 +9,14 @@ export class TodoComponent {
   constructor() {}
 
   private name = 'Ahmet';
-  items = ['item 1', 'item 2', 'item 3'];
+
+  // items = ['item 1', 'item 2', 'item 3', 'item 4'];
+
+  items = [
+    { id: 1, description: 'kahvalti', action: 'yes' },
+    { id: 2, description: 'spor', action: 'yes' },
+    { id: 3, description: 'alisveris', action: 'no' },
+  ];
 
   getName() {
     return this.name;
