d5b910911f4cfa503192c66be7c81056edee905d
ahmetyanik <ahmetyank4242@gmail.com>
2022-11-05 18:14:50 +0100

    template

--- todoapp/src/app/app.component.ts
+++ todoapp/src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  // selector: 'app-root',
-  // selector: '.app-root',
-  //  selector: '#app-root',
-    selector: 'app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app',
+  // templateUrl: './app.component.html',
+
+  // DIKKAT: templateUrl ile template arasindaki fark
+  template: `
+    <h1>App component</h1>
+
+    <h2>{{ title }}</h2>
+    <!-- Degiskeni iki süslü parantez arasina yazarak app.components'ten getirdik -->
+
+    <h2>{{ getTitle() }}</h2>
+
+    <p>{{ todoItem.description }} {{ todoItem.action }}</p>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todoapp';
+  title = 'Todo App';
+
+  todoItem = {
+    description: 'kahvalti',
+    action: true,
+  };
+
+  getTitle() {
+    return this.title;
+  }
 }
