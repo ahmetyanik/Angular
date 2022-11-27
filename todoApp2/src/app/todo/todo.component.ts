063ad3c964a50e925d4f7ac4410039abbe3bd009
ahmetyanik <ahmetyank4242@gmail.com>
2022-11-27 08:15:52 +0100

    ready

--- todoApp2/src/app/todo/todo.component.ts
+++ todoApp2/src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Model } from '../model';
+import { TodoItem } from '../todoitem';
 
 @Component({
   selector: 'app-todo',
@@ -7,15 +8,25 @@ import { Model } from '../model';
   styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent {
-  constructor() {}
+  constructor() {
+    this.model.items = this.getItemsFromLocalStorage();
+
+  }
 
   displayAll: boolean = false;
+  inputText: string = '';
+  deneme : string = "Merhaba"
 
   model = new Model();
 
-  addItem(value: string) {
-    if (value !== '') {
-      this.model.items.push({ description: value, action: false });
+  addItem() {
+    if (this.inputText !== '') {
+      let data = { description: this.inputText, action: false };
+      this.model.items.push(data);
+      let items = this.getItemsFromLocalStorage();
+      items.push(data);
+      localStorage.setItem('items', JSON.stringify(items));
+      this.inputText = '';
     }
   }
 
@@ -30,7 +41,50 @@ export class TodoComponent {
     return this.model.items.filter((item) => item.action === false);
   }
 
-  displayCount(){
-    return this.model.items.filter((item)=>item.action).length;
+  displayCount() {
+    return this.model.items.filter((item) => item.action).length;
+  }
+
+  getBtnClasses() {
+    return {
+      disabled: this.inputText.length === 0,
+      'btn-secondary': this.inputText.length === 0,
+      'btn-primary': this.inputText.length > 0,
+    };
+  }
+
+  getItemsFromLocalStorage() {
+    let items: TodoItem[] = [];
+    let value = localStorage.getItem('items');
+
+    if (value !== null) {
+      items = JSON.parse(value);
+    }
+    return items;
+  }
+
+  onActionChanged(item: TodoItem) {
+    let items = this.getItemsFromLocalStorage();
+
+    localStorage.clear();
+
+    items.forEach(i=>{
+      if(i.description == item.description){
+        i.action = item.action;
+      }
+    })
+    localStorage.setItem('items', JSON.stringify(items));
+  }
+
+  denemeDegistir(){
+
+    if(this.deneme === "Merhaba"){
+      console.log(this.deneme);
+      
+       this.deneme = "Hallo"
+    }else{
+       this.deneme = "Merhaba";
+    }
+
   }
 }
